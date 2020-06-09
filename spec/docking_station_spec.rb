bef0f804a3044aa4666c24fb3bdb48e87d698d93
Matthew Lock <matthew_lock20@hotmail.com>
2020-06-09 17:41:30 +0100

    Step 9.5

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,8 @@ require 'docking_station'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+
+  #it { is_expected.to be_instance_of bike}
+  
+  it expect(:release_bike).to receive(:new)
 end
