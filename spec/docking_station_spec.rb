82362144a4eda20a764adf4220b30de9891b7129
Alessa <alessa_hardwick@live.co.uk>
2020-06-11 13:56:25 +0000

    completed step 10

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,8 +2,15 @@ require 'docking_station'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
-
-  #it { is_expected.to be_instance_of bike}
+    it 'releases working bikes' do
+    bike = subject.release_bike
+    expect(bike).to be_an_instance_of Bike
+  end
   
- # it expect(:release_bike).to receive(:new)
+  describe '#working?' do
+    it 'bike is working' do
+      bike = subject.release_bike
+      expect(bike.working?).to eq(true)
+    end  
+  end  
 end
