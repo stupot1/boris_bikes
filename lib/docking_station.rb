b4f3380a6755f8afe3a05fb59c57dda6b4adbc4e
Miri Michaelson <miriammichaelson@gmail.com>
2020-06-12 15:08:29 +0100

    Completed 13, added docking station capacity

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,10 +1,13 @@
+require_relative  'bike'
+
 class DockingStation
-  attr_reader :bikes
-  
+  attr_reader :bikes, :capacity
+
   def initialize
     @bikes = []
+    @capacity = 1
   end
-  
+
   def release_bike
     if @bikes == []
       raise "no bike available"
@@ -14,8 +17,9 @@ class DockingStation
   end
 
   def dock_bike(bike)
+    return raise "There are no docking spaces available" if @bikes.length == @capacity
     @bikes << bike
     "bike is docked"
   end
-  
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,39 +1,42 @@
 require 'docking_station'
 
 describe DockingStation do
+  let (:bike) {Bike.new}
 
   it { is_expected.to respond_to :release_bike }
 
   describe '#release_bikes' do
     xit 'releases working bikes' do
-      bike = subject.release_bike
-      expect(bike).to be_an_instance_of Bike
+      expect(subject.release_bike).to be_an_instance_of Bike
     end
     it "doesn't release a bike if there are no bikes and raise error" do
       expect {subject.release_bike}.to raise_error("no bike available")
     end
   end
-  
+
   describe '#working?' do
     xit 'bike is working' do
       bike = subject.release_bike
       expect(bike.working?).to eq(true)
-    end  
+    end
   end
 
   describe '#dock_bike' do
     it 'docks working bike' do
-      bike = Bike.new
-      expect(subject.dock_bike(bike)).to eq('bike is docked')  
+      expect(subject.dock_bike(bike)).to eq('bike is docked')
+    end
+
+    it "will not dock bikes if it has hit capacity" do
+      subject.dock_bike(bike)
+      expect {subject.dock_bike(Bike.new)}.to raise_error("There are no docking spaces available")
     end
   end
-  
+
   describe '@bikes' do
     it 'sees what bikes are docked' do
-      bike = Bike.new
       subject.dock_bike(bike)
       expect(subject.bikes[0]).to eq(bike)
-    end  
+    end
   end
 
 
